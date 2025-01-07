4845d6fc4baddbb0da3b2783b1049093cf947af6
Owen Davis <owendavis1991@gmail.com>
2025-01-07 12:20:51 +0000

    ADDED: Added MySQL Configuration and test route

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'yourpassword'
+app.config['MYSQL_DB'] = 'collections'
+
+mysql = MySQL(app)
+
+@app.route('/test_db')
+def test_db():
+    cur = mysql.connection.cursor()
+    cur.execute("SHOW TABLES;")
+    tables = cur.fetchall()
+    cur.close()
+    return str(tables)
